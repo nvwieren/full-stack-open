7c1918319a3aef8a2e22ea248785e268f4ccc56f
Nick van Wieren <nick.van.wieren@enexis.nl>
2025-08-07 20:45:32 +0200

    Updated for 2.5

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,5 +1,22 @@
-import CourseHeader from './CourseHeader'
-import CourseContent from './CourseContent'
+const CourseHeader = ({ name }) => {
+    return <h2>{name}</h2>
+}
+
+const CourseContent = ({ parts }) => {
+    const total = parts.reduce((total, part) => total += part.exercises, 0)
+    return (
+        <>
+            {parts.map(part => 
+                <Part key={part.id} part={part} />
+            )}
+            <p><strong>total of {total} exercises</strong></p>
+        </>
+    )    
+}
+
+const Part = ({ part }) => {
+    return <p key={part.id}>{part.name} {part.exercises}</p>
+}
 
 const Course = ({ course }) => {
     return (
--- part2/courseinfo/src/components/CourseHeader.jsx
+++
@@ -1,5 +0,0 @@
-const CourseHeader = ({ name }) => {
-    return <h2>{name}</h2>
-}
-
-export default CourseHeader
