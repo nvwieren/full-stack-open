039a1e22c824f9e4f71b2d291893a12a780a91b2
Nick van Wieren <2854917+nvwieren@users.noreply.github.com>
2025-10-19 16:59:23 +0200

    part1-6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+  return <button onClick={props.set}>{props.label}</button>
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button label="good" set={handleGoodClick} />
+      <Button label="neutral" set={handleNeutralClick} />
+      <Button label="bad" set={handleBadClick} />
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </div>
+  )
+}
+
+export default App
